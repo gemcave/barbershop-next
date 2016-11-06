fd6fbca39fd5508c18500daf55510c3bdc140000
Stepan Smykovskiy <stevendrosse@gmail.com>
2016-11-06 13:48:03 +0600

    Mistake with toggle

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,9 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var plumber = require('gulp-plumber');
 var server = require('browser-sync');
-var mqpacker = require('css-mqpacker');
-var minify = require('gulp-csso');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer')
-var rename = require('gulp-rename');
-var imagemin = require('gulp-imagemin');
-var svgstore = require('gulp-svgstore');
-var svgmin = require('gulp-svgmin');
 var run = require('run-sequence');
-var del = require('del');
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -24,69 +17,23 @@ gulp.task("style", function() {
       "last 2 Firefox versions",
       "last 2 Opera versions",
       "last 2 Edge versions"
-    ]}),
-    mqpacker({
-      sort: true
-    })
+    ]})
   ]))
-  .pipe(gulp.dest("build/css"))
-  .pipe(minify())
-  .pipe(rename("style.min.css"))
-  .pipe(gulp.dest("build/css"))
+  .pipe(gulp.dest("css"))
   .pipe(server.reload({stream: true}));
-});
-
-gulp.task("images", function() {
-  return gulp.src("build/img/**/*.{png,jpg,gif}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true})
-    ]))
-    .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("symbols", function(){
-  return gulp.src("build/img/icons/*.svg")
-  .pipe(svgmin())
-  .pipe(svgstore({
-    inlineSvg: true
-  }))
-  .pipe(rename("symbols.svg"))
-  .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("serve", function(){
   server.init({
-    server: "build"
+    server: "."
   });
   gulp.watch("less/**/*.less", ["style"]);
   gulp.watch("*.html")
     .on("change", server.reload);
 });
-
-gulp.task("build", function(fn) {
-  run(
-      "clean",
-      "copy",
-      "style",
-      "images",
-      "symbols",
-       fn
-     );
-});
-
-gulp.task("copy", function(){
-  return gulp.src([
-    "fonts/**/*.{woff,woff2}",
-    "img/**",
-    "js/**",
-    "*.html"
-  ], {
-    base: "."
-  })
-  .pipe(gulp.dest("build"));
-});
-
-gulp.task("clean", function(){
-  return del("build");
-});
+  gulp.task("build", function(fn) {
+    run(
+        "style",
+         fn
+       );
+  });
